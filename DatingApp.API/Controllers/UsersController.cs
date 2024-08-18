d99b7ab2aac0bb34386f741ba6e7353aeac84eb2
Bharath kumar reddy kummetha <kbkr033@gmail.com>
2024-08-18 22:36:56 +0530

    adding initial .net controller

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.API.Data;
+using DatingApp.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController(DataContext dataContext) : ControllerBase
+    {
+        private readonly DataContext _dataContext = dataContext;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var user = await _dataContext.Users.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            return user;
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using DatingApp.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
