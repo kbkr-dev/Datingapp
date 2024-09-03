98a74c8581c80910c73e77c38e89d8cc5a9e1e50
Bharath kumar reddy kummetha <kbkr033@gmail.com>
2024-09-03 20:34:52 +0530

    angular routing

--- DatingApp.Client/src/app/app.routes.ts
+++ DatingApp.Client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_gaurds/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component: MemberListComponent, canActivate: [authGuard]},
+            {path: 'members/:id', component: MemberDetailComponent, canActivate: [authGuard]},
+            {path: 'lists', component: ListsComponent, canActivate: [authGuard]},
+            {path: 'messages', component: MessagesComponent, canActivate: [authGuard]}
+        ]
+    },    
+    {path: '**', component: HomeComponent, pathMatch: "full"}
+];
--- DatingApp.Client/src/app/register/register.component.ts
+++ DatingApp.Client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, input, Output, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -13,6 +14,7 @@ export class RegisterComponent {
 model: any = {};
 accountService = inject(AccountService);
 cancelEvent = output<boolean>();
+private toastr = inject(ToastrService);
 
 register(){
   this.accountService.register(this.model).subscribe({
@@ -20,8 +22,8 @@ register(){
       console.log(res);
       this.cancel();
     },
-    error: error => console.log(error),
-    complete: () => console.log("completed")
+    error: error => this.toastr.error(error.error),
+    complete: () => this.toastr.success("registered")
   })
 }
 
