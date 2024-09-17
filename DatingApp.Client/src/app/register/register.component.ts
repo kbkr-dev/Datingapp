ac3cf369f81709d7fdfaf4a8286b039510266beb
Bharath kumar reddy kummetha <kbkr033@gmail.com>
2024-09-17 17:57:26 +0530

    photo upload and delete

--- DatingApp.Client/src/app/register/register.component.ts
+++ DatingApp.Client/src/app/register/register.component.ts
@@ -1,30 +1,57 @@
-import { Component, EventEmitter, inject, input, Output, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, inject, input, OnInit, Output, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { JsonPipe, NgIf } from '@angular/common';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
 model: any = {};
 accountService = inject(AccountService);
 cancelEvent = output<boolean>();
 private toastr = inject(ToastrService);
+registerForm: FormGroup = new FormGroup({});
+private fb = inject(FormBuilder);
+
+ngOnInit(): void {
+  this.InitializeForm();
+}
+
+InitializeForm(){
+  this.registerForm = new FormGroup({
+    username: new FormControl('Hello', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+    confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+  });
+  this.registerForm.controls['password'].valueChanges.subscribe({
+    next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+  })
+}
+
+matchValues(matchTo: string): ValidatorFn{
+  return (control: AbstractControl) => {
+    return control.value == control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+  }
+}
 
 register(){
-  this.accountService.register(this.model).subscribe({
-    next: res => {
-      console.log(res);
-      this.cancel();
-    },
-    error: error => this.toastr.error(error.error),
-    complete: () => this.toastr.success("registered")
-  })
+  console.log(this.registerForm.value);
+  // this.accountService.register(this.model).subscribe({
+  //   next: res => {
+  //     console.log(res);
+  //     this.cancel();
+  //   },
+  //   error: error => this.toastr.error(error.error),
+  //   complete: () => this.toastr.success("registered")
+  // })
 }
 
 cancel(){
